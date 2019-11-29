ee8e1e90abcaa280f6f1330d018abbc7ccfbcd19
rakaar <rka87338@gmail.com>
2019-11-29 13:09:51 +0530

    finalised proj2.js

--- project2.js
+++ project2.js
@@ -1,7 +1,8 @@
 var x = require('./stage1');
 
 const axios = require('axios');
-
+const fs = require('fs');
+var x1 = [];
 x.forEach(ele => {
 
   let mentor = ele.link.split('/')[3];
@@ -9,7 +10,20 @@ x.forEach(ele => {
   // console.log('link is ',repo)
   axios
     .get(`https://api.github.com/repos/${mentor}/${repo}`, {headers: { 'Accept':  "application/vnd.github.mercy-preview+json"}})
-    .then(res => console.log('dam ', res.data.topics))
-    .catch(err => console.log(err))
+    .then(res => {
+      console.log('dam ', res.data.topics);
+      x1.push({
+        ...ele,
+        "tag": res.data.topics 
+      })
+    })
+    .catch(err => {
+      console.log(err);
+      x1.push({
+        ...ele,
+        "tag": []
+      })
+    })
 })
 
+fs.writeFile('stage2.js', x1, err => console.log(err));
